3eca5d643d93d50e9bdfae44f3f5c3831c3d8bcd
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-04-04 13:39:44 +0200

    Add files via upload

--- script.php
+++ script.php
@@ -3,10 +3,14 @@
 // import de la classe Impot
 include 'model/Impot.php';
 
+// on convertit l'objet JSON obtenu dans $_POST (inutilisable sinon)
+// à commenter si on utilise la V2
+$_POST = json_decode(file_get_contents('php://input'), true);
+
 // création d'un objet impôt avec les valeurs des inputs dans $_POST
 $impot = new Impot($_POST['name'], $_POST['income']);
 
-// calcul de l'impôt
+// calcul de l'impôt et stockage du message
 $msgMontant = $impot->afficheImpot();
 
 // puis affichage du message
